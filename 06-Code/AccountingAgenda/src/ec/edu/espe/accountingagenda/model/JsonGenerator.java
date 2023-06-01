494985823880caf7b83f8f16cadd42cdcefb35a8
mirandaalison <mirandacreamer@gmail.com>
2023-06-01 02:25:57 -0500

    V0.5

--- 06-Code/AccountingAgenda/src/ec/edu/espe/accountingagenda/model/JsonGenerator.java
+++ 06-Code/AccountingAgenda/src/ec/edu/espe/accountingagenda/model/JsonGenerator.java
@@ -1,11 +1,22 @@
 package ec.edu.espe.accountingagenda.model;
 
-/**
- *
- * @author Bug Busters, DCCO-ESPE
- * @version V0.1
- */
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class JsonGenerator {
-    
+    private static final String FILE_NAME = "agenda.json";
+
+    public static void generateJson(Calendar calendar) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+        try (FileWriter writer = new FileWriter(FILE_NAME)) {
+            gson.toJson(calendar, writer);
+            System.out.println("Archivo JSON generado exitosamente.");
+        } catch (IOException e) {
+            System.out.println("Error al generar el archivo JSON: " + e.getMessage());
+        }
+    }
 }
--- 06-Code/AccountingAgenda/src/ec/edu/espe/accountingagenda/view/PrincipalMenu.java
+++ 06-Code/AccountingAgenda/src/ec/edu/espe/accountingagenda/view/PrincipalMenu.java
@@ -1,70 +1,66 @@
 package ec.edu.espe.accountingagenda.view;
 
+import ec.edu.espe.accountingagenda.model.Calendar;
+import ec.edu.espe.accountingagenda.model.JsonGenerator;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/**
- *
- * @author Bug Busters, DCCO-ESPE
- * @version V0.1
- */
-
 public class PrincipalMenu {
     public void showPrincipalMenu() {
-    Scanner scanner = new Scanner(System.in);
-    int option = 0;
+        Scanner scanner = new Scanner(System.in);
+        int option = 0;
+        Calendar calendar = new Calendar(); // Instancia de la clase Calendar
 
-    do {
-        try {
-            System.out.println("---------- MENU PRINCIPAL ----------");
-            System.out.println("1. Notas");
-            System.out.println("2. Pendientes");
-            System.out.println("3. Generar archivo JSON");
-            System.out.println("4. Salir");
-            System.out.print("Ingrese la opción que desea visualizar: ");
+        do {
+            try {
+                System.out.println("---------- MENU PRINCIPAL ----------");
+                System.out.println("1. Notas");
+                System.out.println("2. Pendientes");
+                System.out.println("3. Generar archivo JSON");
+                System.out.println("4. Salir");
+                System.out.print("Ingrese la opción que desea visualizar: ");
 
-            if (scanner.hasNextInt()) {
-                option = scanner.nextInt();
-                scanner.nextLine();
+                if (scanner.hasNextInt()) {
+                    option = scanner.nextInt();
+                    scanner.nextLine();
 
-                switch (option) {
-                    case 1:
-                        NoteMenu noteMenu = new NoteMenu();
-                        noteMenu.showNoteMenu();
-                        break;
-                    case 2:
-                        CalendarMenu calendarMenu = new CalendarMenu();
-                        calendarMenu.showCalendarMenu();
-                        break;
-                    case 3:
-                        // Generar archivo JSON
-                        break;
-                    case 4:
-                        System.out.println("Salió del programa exitosamente");
-                        break;
-                    default:
-                        System.out.println("Opción inválida, ingrese de nuevo: ");
-                        break;
+                    switch (option) {
+                        case 1:
+                            NoteMenu noteMenu = new NoteMenu();
+                            noteMenu.showNoteMenu();
+                            break;
+                        case 2:
+                            CalendarMenu calendarMenu = new CalendarMenu();
+                            calendarMenu.showCalendarMenu();
+                            break;
+                        case 3:
+                            JsonGenerator.generateJson(calendar);
+                            break;
+                        case 4:
+                            System.out.println("Salió del programa exitosamente");
+                            break;
+                        default:
+                            System.out.println("Opción inválida, ingrese de nuevo: ");
+                            break;
+                    }
+                } else {
+                    String input = scanner.nextLine();
+                    System.out.println("Error: La entrada '" + input + "' no es un número válido.");
                 }
-            } else {
-                String input = scanner.nextLine();
-                System.out.println("Error: La entrada '" + input + "' no es un número válido.");
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Opción inválida. Debe ingresar un número.");
+                scanner.nextLine();
+            } catch (NumberFormatException e) {
+                System.out.println("Error: La entrada debe ser un número válido.");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Error de argumento: " + e.getMessage());
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Error: Opción inválida. Debe ingresar un número.");
-            scanner.nextLine();
-        } catch (NumberFormatException e) {
-            System.out.println("Error: La entrada debe ser un número válido.");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error de argumento: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
 
-        System.out.println();
-    } while (option != 4);
+            System.out.println();
+        } while (option != 4);
 
-    scanner.close(); 
-}
-    
+        scanner.close(); 
+    }
 }
