1fb652b3929199a07aba0337c08697b330d1b603
Edison Ludeña <premusic314@gmail.com>
2023-05-31 21:57:10 -0500

    V0.2.1

--- 06-Code/AccountingAgenda/src/ec/edu/espe/accountingagenda/view/NoteMenu.java
+++ 06-Code/AccountingAgenda/src/ec/edu/espe/accountingagenda/view/NoteMenu.java
@@ -11,11 +11,14 @@ import java.util.Scanner;
  */
 
 public class NoteMenu {
+
     public void showNoteMenu() {
-        Scanner scanner = new Scanner(System.in);
-        int option;
-        Note note = new Note();
-        do {
+    Scanner scanner = new Scanner(System.in);
+    int option = 0;
+    Note note = new Note();
+
+    do {
+        try {
             System.out.println("---------- MENU DE NOTAS ----------");
             System.out.println("1. Crear Nota");
             System.out.println("2. Editar Nota");
@@ -24,47 +27,49 @@ public class NoteMenu {
             System.out.println("5. Borrar Nota");
             System.out.println("6. Volver al Menú Principal");
             System.out.print("Ingrese la opción que desea visualizar: ");
-            option = scanner.nextInt();
-            scanner.nextLine(); 
-            
-            try{
-            switch (option) {
-                case 1: 
-                    note.createNote();
-                break;
-                case 2:
-                    note.editNote();
-                break;
-                case 3:
-                    note.readNote();
-                break;
-                case 4:
-                    note.searchNote(); 
-                break;
-                case 5:
-                    note.deleteNote(); 
-                break;
-                case 6:
-                return;
-                default:
-                    System.out.println("Opción inválida, ingrese de nuevo: ");
-                    break;
-            }
-            
-                } catch (NumberFormatException e) {
-                    System.out.println("Error: La entrada debe ser un número válido.");
-                }catch (InputMismatchException e) {
-                    System.out.println("Error: Opción inválida. Debe ingresar un número.");
-                    scanner.nextLine();
-                }catch (IllegalArgumentException e) {
-                    System.out.println("Error de argumento: " + e.getMessage());
-                }catch(Exception e){
-                    System.out.println("Error: " + e.getMessage());
+
+            if (scanner.hasNextInt()) {
+                option = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (option) {
+                    case 1:
+                        note.createNote();
+                        break;
+                    case 2:
+                        note.editNote();
+                        break;
+                    case 3:
+                        note.readNote();
+                        break;
+                    case 4:
+                        note.searchNote();
+                        break;
+                    case 5:
+                        note.deleteNote();
+                        break;
+                    case 6:
+                        return;
+                    default:
+                        System.out.println("Opción inválida, ingrese de nuevo: ");
+                        break;
                 }
+            } else {
+                String input = scanner.nextLine();
+                System.out.println("Error: La entrada '" + input + "' no es un número válido.");
+            }
 
             System.out.println();
-        } while (option != 6);
-    }
+        } catch (InputMismatchException e) {
+            System.out.println("Error: La entrada debe ser un número válido.");
+            scanner.nextLine();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    } while (option != 6);
+}
+    
+    
 }
 
 
--- 06-Code/AccountingAgenda/src/ec/edu/espe/accountingagenda/view/PrincipalMenu.java
+++ 06-Code/AccountingAgenda/src/ec/edu/espe/accountingagenda/view/PrincipalMenu.java
@@ -11,52 +11,59 @@ import java.util.Scanner;
 
 public class PrincipalMenu {
     public void showPrincipalMenu() {
-        Scanner scanner = new Scanner(System.in);
-        int option;
+    Scanner scanner = new Scanner(System.in);
+    int option = 0;
 
-        do {
-            
+    do {
+        try {
             System.out.println("---------- MENU PRINCIPAL ----------");
             System.out.println("1. Notas");
             System.out.println("2. Pendientes");
             System.out.println("3. Generar archivo JSON");
             System.out.println("4. Salir");
             System.out.print("Ingrese la opción que desea visualizar: ");
-            option = scanner.nextInt();
-            scanner.nextLine();
-            
-            try{
-            
-            switch (option) {
-                case 1:
-                    NoteMenu noteMenu = new NoteMenu();
-                    noteMenu.showNoteMenu();
-                    break;
-                case 2:
-                    // Pendientes 
-                    break;
-                case 3:
-                    // Generar archivo JSON 
-                    break;
-                case 4:
-                    System.out.println("Salió del programa exitosamente");
-                    break;
-                default:
-                    System.out.println("Opción inválida, ingrese de nuevo: ");
-                    break;
-            }
-            
-                } catch (NumberFormatException e) {
-                    System.out.println("Error: La entrada debe ser un número válido.");
-                }catch (InputMismatchException e) {
-                    System.out.println("Error: Opción inválida. Debe ingresar un número.");
-                    scanner.nextLine();
-                }catch (IllegalArgumentException e) {
-                    System.out.println("Error de argumento: " + e.getMessage());
-                }catch(Exception e){
-                    System.out.println("Error: " + e.getMessage());
+
+            if (scanner.hasNextInt()) {
+                option = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (option) {
+                    case 1:
+                        NoteMenu noteMenu = new NoteMenu();
+                        noteMenu.showNoteMenu();
+                        break;
+                    case 2:
+                        // Pendientes
+                        break;
+                    case 3:
+                        // Generar archivo JSON
+                        break;
+                    case 4:
+                        System.out.println("Salió del programa exitosamente");
+                        break;
+                    default:
+                        System.out.println("Opción inválida, ingrese de nuevo: ");
+                        break;
                 }
-            System.out.println();
-        } while (option != 4);
-    }
+            } else {
+                String input = scanner.nextLine();
+                System.out.println("Error: La entrada '" + input + "' no es un número válido.");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Opción inválida. Debe ingresar un número.");
+            scanner.nextLine();
+        } catch (NumberFormatException e) {
+            System.out.println("Error: La entrada debe ser un número válido.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error de argumento: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        System.out.println();
+    } while (option != 4);
+
+    scanner.close(); // Cerrar el scanner al finalizar
+}
+    
 }
